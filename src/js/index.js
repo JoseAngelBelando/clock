044dbd1990961fa4a9fa13b7b19664227356ee47
joseAngel33 <163841769+joseAngel33@users.noreply.github.com>
2024-07-07 09:12:17 +0200

    finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-
+// Localizamos los id
 const hoursElement = document.getElementById('hours');
 const minutesElement = document.getElementById('minutes');
 const dayOfWeekElement = document.getElementById('day-of-week');
@@ -9,9 +9,9 @@ const yearElement = document.getElementById('year');
 const hoursHandElement = document.getElementById('hour-hand');
 const minutesHandElement = document.getElementById('minute-hand');
 const secondsHandElement = document.getElementById('second-hand');
-
+// creamos una aray con los dias
 const days = ['Sunday', 'Monday', 'Tusday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
+// creamos un array con los mese
 const months = [
   'January',
   'February',
@@ -26,11 +26,12 @@ const months = [
   'November',
   'December'
 ];
-
+// definimos una funcion que agrega un 0 delante de un número si es menor a 10.
 const fixTime = number => (number < 10 ? '0' + number : number);
+// las funciones de "getDayOfWeek y getMonth" que devuelven el nombre del día y del mes.
 const getDayOfWeek = today => days[today];
 const getMonth = currentMonth => months[currentMonth];
-
+// la funcion setClocks se encarga de obtener la fecha y hora actual, actualizar los elementos del reloj en el DOM con esta información, y rotar las manecillas del reloj según la hora, minutos y segundos actuales.
 const setClocks = () => {
   const now = new Date();
   const hours = now.getHours();
@@ -58,7 +59,8 @@ const setClocks = () => {
   const hourDegrees = hours * 30 + minutes / 2;
   hoursHandElement.style.transform = `rotate(${hourDegrees}deg)`;
 };
-
+// setInterval se utilaza para  llamar a la función
+// setClocks  para mostrar la hora actual al cargar la página.
 setInterval(() => {
   setClocks();
 }, 1000);
